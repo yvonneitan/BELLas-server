1a5b202da899c4ac49f237077545786a0407c8f8
Yvonne Itan <68035004+yvonneitan@users.noreply.github.com>
2025-02-17 17:37:24 -0700

    Update server.js
    -Reverse back to the original server file content

--- server.js
+++ server.js
@@ -1,44 +1,15 @@
-// import express from "express";
-// import cors from "cors";
-// import "dotenv/config";
-// import suggestionsRoutes from "./routes/suggestions-routes.js";
-// import votesRoutes from "./routes/votes-routes.js";
-
-// const app = express();
-
-// const PORT = process.env.PORT || 8080;
-// const BACKEND_URL = process.env.BACKEND_URL || `http://localhost:${PORT}`;
-
-// app.use(cors());
-// app.use(express.static("public"));
-// app.use(express.json());
-
-// app.get("/", (_req, res) => {
-//   res.send("Welcome to the Bell Changemakers server");
-// });
-
-// app.use("/suggestions", suggestionsRoutes);
-// app.use("/votes", votesRoutes);
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on ${BACKEND_URL}${PORT}`);
-// });
-import cors from "cors";
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 import suggestionsRoutes from "./routes/suggestions-routes.js";
 import votesRoutes from "./routes/votes-routes.js";
 
 const app = express();
 
-// CORS configuration for allowing requests from your Netlify frontend
-const allowedOrigins = ["https://your-netlify-app.netlify.app"]; // Replace with your actual Netlify URL
-const corsOptions = {
-  origin: allowedOrigins,
-  methods: "GET, POST, PUT, DELETE",
-};
+const PORT = process.env.PORT || 8080;
+const BACKEND_URL = process.env.BACKEND_URL || `http://localhost:${PORT}`;
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.static("public"));
 app.use(express.json());
 
@@ -49,9 +20,6 @@ app.get("/", (_req, res) => {
 app.use("/suggestions", suggestionsRoutes);
 app.use("/votes", votesRoutes);
 
-const PORT = process.env.PORT || 8080;
-const BACKEND_URL = process.env.BACKEND_URL || `http://localhost:${PORT}`;
-
 app.listen(PORT, () => {
-  console.log(`Server running on ${BACKEND_URL}`);
+  console.log(`Server running on ${BACKEND_URL}${PORT}`);
 });
